f8d6af7547a181bb4005156137e193dd253a3ea9
thomasjoel <12jt42@queensu.ca>
2018-10-30 17:31:54 -0500

    added classes for user jobs

---
+++ TaskManagement/UserMyJobViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserMyJobViewController.swift
+//  TaskManagement
+//
+//  Created by Team1 on 10/30/18.
+//  Copyright © 2018 Team1UHCosc4355. All rights reserved.
+//
+
+import UIKit
+
+class UserMyJobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
